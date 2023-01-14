da56406306661ca8a8aed65492cb71bb26382898
Damon German <damon.german@hyland.com>
2023-01-14 11:24:35 -0600

    made some changes

---
+++ LatestCsharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LatestCsharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var point = new Point { X = 1.0, Y = 2.0 };
+            Console.WriteLine($"({point.X}, {point.Y})");
+        }
+    }
+}
